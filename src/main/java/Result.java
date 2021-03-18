74f29d7be8aab8f5fa924745c27f7312dbb327da
Martin2696 <mato.siska@gmail.com>
2021-03-18 22:02:19 +0100

    final commit

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -1,50 +1,26 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Result {
 
+
     public static List<Integer> gradingStudents(List<Integer> grades) {
 
-        List<Integer> studentGrades = new ArrayList<>();
+        List<Integer> studentGrades = new ArrayList<>(grades);
 
-        for(int i = 0; i < grades.size(); i++) {
+        if (grades.get(0) < 1 || grades.get(0) > 60) {
+            return new ArrayList<>();
+        }
 
-            if(grades.get(i) > 1 && grades.get(i) < 38 ) {
-                studentGrades.add(grades.get(i));
-            } else if (grades.get(i) < 100 && grades.get(i) >= 38) {
-                int nextMultiple = 5 - (grades.get(i) % 5) + grades.get(i);
-                if(nextMultiple - grades.get(i) < 3) {
-                    studentGrades.add(nextMultiple);
-                } else {
-                    studentGrades.add(grades.get(i));
-                }
+        for (int i = 1; i <= grades.get(0); i++) {
+            if (grades.get(i) > 100 || grades.get(i) < 1) {
+                studentGrades.set(i, 0);
+
+            } else if(grades.get(i) >= 38 && grades.get(i) % 5 >= 3 && grades.get(i) % 10 >= 3) {
+
+                studentGrades.set(i, grades.get(i) + 5 - grades.get(i) % 5);
             }
         }
         return studentGrades;
-    }
-
-    public static void main(String[] args) {
-
-        int n;
-        Scanner sc = new Scanner(System.in) ;
-
-        System.out.println("Number of grades: ");
-        n = sc.nextInt() ;
-        List<Integer> grades = new ArrayList<>() ;
-
-        for(int i=0; i < n; i++ )
-        {
-            int temp;
-            System.out.println("Next Number: ");
-            temp = sc.nextInt();
-            grades.add(temp) ;
-            if (grades.get(i) > 100  || grades.get(i) < 1) {
-                grades.remove(i);
-                i--;
-            }
-            System.out.println(gradingStudents(grades));
-        }
-        gradingStudents(grades);
     }
 }
--- src/test/java/ResultCheck.java
+++ src/test/java/ResultCheck.java
@@ -3,11 +3,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 /**
- * GitHub URL: https://github.com/Martin2696/prog2-ss21-exercise1
+ * GitHub URL: https://github.com/Martin2696/prog2-ss21-exercise1.git
  * Test class for testing the gradingStudents method in Result.java
  */
 
@@ -24,26 +23,108 @@ public class ResultCheck {
     }
 
     @Test
-    @DisplayName("Test 1")
-    void testResult1() {
-        List<Integer> actual = new ArrayList<>(Arrays.asList(3, 38, 57, 69));
-        List<Integer> expected = new ArrayList<>(Arrays.asList(3, 40, 57, 70));
-        assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(actual).toArray()), "Code doesn't work properly");
+    @DisplayName("test 1 - is the list empty?")
+    public void testGradingStudents_EmptyList() {
+        List<Integer> grades = new ArrayList<>();
+        List<Integer> expected = new ArrayList<>();
+
+        assertEquals(expected, grades);
     }
 
     @Test
-    @DisplayName("Test 2")
-    void testResult2() {
-        List<Integer> actual = new ArrayList<>(Arrays.asList(1, 48));
-        List<Integer> expected = new ArrayList<>(Collections.singletonList(50));
-        assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(actual).toArray()), "Code doesn't work properly");
+    @DisplayName("test 2 - are there more than 60 students in the list")
+    public void testGradingStudents_ListOfMoreThan_60_Students() {
+        List<Integer> grades = Result.gradingStudents(Arrays.asList(65, 2, 4, 6, 8, 10, 40, 42, 44, 46, 48, 50, 71, 73, 75, 77, 79, 81, 82, 83,
+                84, 85, 86, 87, 88, 89, 90, 45, 54, 66, 55, 44, 33, 22, 11, 1, 37, 38, 39, 40, 67, 97, 98, 91, 91, 75, 85,
+                84, 83, 82, 67, 34, 81, 41, 68, 69, 94, 91, 87, 46, 87, 20, 98, 99 , 42, 100));
+        List<Integer> expected = new ArrayList<>();
+
+        assertEquals(expected, grades);
     }
 
     @Test
-    @DisplayName("Test 3")
-    void testResult3() {
-        List<Integer> actual = new ArrayList<>(Arrays.asList(8, 99, 89, 78, 68, 67, 46, 39, 24));
-        List<Integer> expected = new ArrayList<>(Arrays.asList(8, 100, 90, 80, 70, 67, 46, 40, 24));
-        assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(actual).toArray()), "Code doesn't work properly");
+    @DisplayName("test 3 - is the number of students negative?")
+    public void testGradingStudents_NegativeNumberOfStudents() {
+        List<Integer> grades = Result.gradingStudents(Arrays.asList(-1, 37, 38));
+        List<Integer> expected = new ArrayList<>();
+
+        assertEquals(expected, grades);
+    }
+
+    @Test
+    @DisplayName("test 4 - are the grades being round up?")
+    public void testGradingStudents_RoundingUp() {
+        List<Integer> grades = Result.gradingStudents(Arrays.asList(5, 74, 68, 98, 38, 59));
+        List<Integer> expected = Arrays.asList(5, 75, 70, 100, 40, 60);
+
+        assertTrue(expected.size() == grades.size()
+                && expected.containsAll(grades)
+                && grades.containsAll(expected));
+    }
+
+    @Test
+    @DisplayName("test 5 - are the points passed trouble-free?")
+    public void testGradingStudents_NothingUnusual() {
+        List<Integer> grades = Result.gradingStudents(Arrays.asList(5, 62, 72, 81, 97, 42));
+        List<Integer> expected = Arrays.asList(5, 62, 72, 81, 97, 42);
+
+        assertTrue(expected.size() == grades.size()
+                && expected.containsAll(grades)
+                && grades.containsAll(expected));
+    }
+
+    @Test
+    @DisplayName("test 6 - is the line for negative grade rounding up properly?")
+    public void testGradingStudents_NegativeGradeTest() {
+        List<Integer> grades = Result.gradingStudents(Arrays.asList(4, 36, 37, 38, 39));
+        List<Integer> expected = Arrays.asList(4, 36, 37, 40, 40);
+
+        assertTrue(expected.size() == grades.size()
+                && expected.containsAll(grades)
+                && grades.containsAll(expected));
+    }
+
+    @Test
+    @DisplayName("test 7 - does only one student in the list work?")
+    public void testGradingStudents_OnlyOneStudent() {
+        List<Integer> grades = Result.gradingStudents(Arrays.asList(1, 99));
+        List<Integer> expected = Arrays.asList(1, 100);
+
+        assertTrue(expected.size() == grades.size()
+                && expected.containsAll(grades)
+                && grades.containsAll(expected));
+    }
+
+    @Test
+    @DisplayName("test 8 - did somebody score more than 100?")
+    public void testGradingStudents_MoreThan_100_Points() {
+        List<Integer> grades = Result.gradingStudents(Arrays.asList(1, 1800));
+        List<Integer> expected = Arrays.asList(1, 0);
+
+        assertTrue(expected.size() == grades.size()
+                && expected.containsAll(grades)
+                && grades.containsAll(expected));
+    }
+
+    @Test
+    @DisplayName("test 9 - did more students score more than 100?")
+    public void testGradingStudents_MoreStudentsMoreThan_100_Points() {
+        List<Integer> grades = Result.gradingStudents(Arrays.asList(4, 111, 112, 101, 102));
+        List<Integer> expected = Arrays.asList(4, 0, 0, 0 ,0);
+
+        assertTrue(expected.size() == grades.size()
+                && expected.containsAll(grades)
+                && grades.containsAll(expected));
+    }
+
+    @Test
+    @DisplayName("test 10 - somebody got less than 0 points?")
+    public void testGradingStudents_NegativePoints() {
+        List<Integer> grades = Result.gradingStudents(Arrays.asList(2, -5, -97));
+        List<Integer> expected = Arrays.asList(2, 0, 0);
+
+        assertTrue(expected.size() == grades.size()
+                && expected.containsAll(grades)
+                && grades.containsAll(expected));
     }
 }
