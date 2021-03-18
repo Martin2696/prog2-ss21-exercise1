c2e7f362604ecf302f0e18dc300583e7c69572fc
Martin2696 <mato.siska@gmail.com>
2021-03-18 12:24:56 +0100

    First commit with the class Result, test cases will be added soon

---
+++ src/main/java/Result.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Result {
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+
+        List<Integer> studentGrades = new ArrayList<>();
+
+        for(int i = 0; i < grades.size(); i++) {
+
+            if(grades.get(i) > 1 && grades.get(i) < 38 ) {
+                studentGrades.add(grades.get(i));
+            } else if (grades.get(i) < 100 && grades.get(i) >= 38) {
+                int nextMultiple = 5 - (grades.get(i) % 5) + grades.get(i);
+                if(nextMultiple - grades.get(i) < 3) {
+                    studentGrades.add(nextMultiple);
+                } else if (grades.get(i) > 100  || grades.get(i) < 1) {
+                    studentGrades.remove(i);
+                    i--;
+                } else {
+                    studentGrades.add(grades.get(i));
+                }
+            }
+        }
+        return studentGrades;
+    }
+
+    /* public static void main(String[] args) {
+
+        int n;
+        Scanner sc = new Scanner(System.in) ;
+
+        System.out.println("Number of grades: ");
+        n = sc.nextInt() ;
+        List<Integer> grades = new ArrayList<>() ;
+
+        for(int i=0; i < n; i++ )
+        {
+            int temp;
+            System.out.println("Next Number: ");
+            temp = sc.nextInt();
+            grades.add(temp) ;
+            if (grades.get(i) > 100  || grades.get(i) < 1) {
+                grades.remove(i);
+                i--;
+            }
+            System.out.println(gradingStudents(grades));
+        }
+        gradingStudents(grades);
+    } */
+}
---
+++ src/test/java/ResultCheck.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.*;
+
+/**
+ * GitHub URL: https://github.com/Martin2696/prog2-ss21-exercise1.git
+ * Test class for testing the gradingStudents method in Result.java
+ */
+
+public class ResultCheck {
+
+    @BeforeAll
+    public static void before() {
+        System.out.println("Testing");
+    }
+
+    @AfterAll
+    public static void after() {
+        System.out.println("Done with testing");
+    }
+
+    @Test
+    @DisplayName("Test 1 ")
+    void testResult_1() {
+
+    }
+}
